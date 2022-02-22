c684041056f96d7be1936ebe3db94c77b08bf9b6
김지웅 <godjw96@gmail.com>
2022-02-23 00:11:37 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
-import "express-async-error";
+import "express-async-errors";
 import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(morgan());
 
 app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -21,4 +24,4 @@ app.use((error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
-app.listen(8080);
+app.listen(config.host.port);
