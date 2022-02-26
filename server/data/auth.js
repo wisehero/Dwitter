452b6b3689a16e429150dadb920cf31f5c357258
김지웅 <godjw96@gmail.com>
2022-02-26 19:57:40 +0900

    Update auth.js

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,24 +1,55 @@
-let users = [
+import SQ from "sequelize";
+import { db, sequelize } from "../db/database.js";
+const DataTypes = SQ.DataTypes;
+
+const User = sequelize.define(
+  "user",
   {
-    id: "1",
-    username: "bob",
-    password: "$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm",
-    name: "Bob",
-    email: "bob@gmail.com",
-    url: "https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png",
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    url: DataTypes.TEXT,
   },
-];
+  { timestamps: false }
+);
 
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  return db
+    .execute("SELECT * FROM users WHERE username=?", [username])
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute("SELECT * FROM users WHERE id=?", [id])
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      "INSERT INTO users (username, password, name, email, url) VALUES(?,?,?,?,?)",
+      [username, password, name, email, url]
+    )
+    .then((result) => result[0].insertId);
 }
