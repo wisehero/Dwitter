4e6eca614a5eb43ed4ccae9f9d462319e3a2f047
김지웅 <godjw96@gmail.com>
2022-02-27 00:08:59 +0900

    로깅 해제

--- server/db/database.js
+++ server/db/database.js
@@ -6,6 +6,7 @@ const { host, user, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
   dialect: "mysql",
+  logging: false,
 });
 
 const pool = mysql.createPool({
