158094d933d85a90d472797c3633b0a8c5a5370f
김지웅 <godjw96@gmail.com>
2022-02-26 19:57:23 +0900

    데이터베이스 시퀄라이즈 셋팅

---
+++ server/db/database.js
@@ -0,0 +1,18 @@
+import { config } from "../config.js";
+import mysql from "mysql2";
+import SQ from "sequelize";
+
+const { host, user, database, password } = config.db;
+export const sequelize = new SQ.Sequelize(database, user, password, {
+  host,
+  dialect: "mysql",
+});
+
+const pool = mysql.createPool({
+  host: config.db.host,
+  user: config.db.user,
+  database: config.db.database,
+  password: config.db.password,
+});
+
+export const db = pool.promise();
