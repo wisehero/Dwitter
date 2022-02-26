a1965b319c1e63cecba2716fbe6b38991d672e34
김지웅 <godjw96@gmail.com>
2022-02-27 00:08:49 +0900

    Update auth.js

--- server/data/auth.js
+++ server/data/auth.js
@@ -2,7 +2,7 @@ import SQ from "sequelize";
 import { db, sequelize } from "../db/database.js";
 const DataTypes = SQ.DataTypes;
 
-const User = sequelize.define(
+export const User = sequelize.define(
   "user",
   {
     id: {
@@ -33,23 +33,15 @@ const User = sequelize.define(
 );
 
 export async function findByUsername(username) {
-  return db
-    .execute("SELECT * FROM users WHERE username=?", [username])
-    .then((result) => result[0][0]);
+  return User.findOne({ where: { username } });
 }
 
 export async function findById(id) {
-  return db
-    .execute("SELECT * FROM users WHERE id=?", [id])
-    .then((result) => result[0][0]);
+  return User.findByPk(id);
 }
 
 export async function createUser(user) {
-  const { username, password, name, email, url } = user;
-  return db
-    .execute(
-      "INSERT INTO users (username, password, name, email, url) VALUES(?,?,?,?,?)",
-      [username, password, name, email, url]
-    )
-    .then((result) => result[0].insertId);
+  return User.create(user).then((data) => {
+    data.dataValues.id;
+  });
 }
