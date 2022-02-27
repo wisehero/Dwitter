49b2a4803a656207405b3601fa64c54b93fdc08c
김지웅 <godjw96@gmail.com>
2022-02-27 22:34:04 +0900

    MongoDB tweet.js 적용

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,12 +1,23 @@
+import MongoDb from "mongodb";
+import { getTweets } from "../database/database.js";
+import * as userRepository from "./auth.js";
+
+const ObjectId = MongoDb.ObjectId;
+
+export async function getAll() {
+  return getTweets() //
+    .find()
+    .sort({ createdAt: -1 })
+    .toArray()
+    .then(mapTweets);
+}
+
 export async function getAllByUsername(username) {
-  return Tweet.findAll({
-    ...INCLUDE_USER,
-    ...ORDER_DESC,
-    include: {
-      ...INCLUDE_USER.include,
-      where: { username },
-    },
-  });
+  return getTweets() //
+    .find()
+    .sort({ createdAt: -1 })
+    .toArray()
+    .then(mapTweets);
 }
 
 export async function getById(id) {
@@ -17,20 +28,44 @@ export async function getById(id) {
 }
 
 export async function create(text, userId) {
-  return Tweet.create({ text, userId }).then((data) =>
-    this.getById(data.dataValues.id)
-  );
+  const { name, username, url } = await userRepository.findById(userId);
+  const tweet = {
+    text,
+    createdAt: new Date(),
+    userId,
+    name: name,
+    username: username,
+    url: url,
+  };
+  return getTweets()
+    .insertOne(tweet)
+    .then((data) => {
+      mapOptionalTweet({
+        ...tweet,
+        _id: data.insertedId,
+      });
+    });
 }
 
 export async function update(id, text) {
-  return Tweet.findByPk(id, INCLUDE_USER).then((tweet) => {
-    tweet.text = text;
-    return tweet.save();
-  });
+  return getTweets()
+    .findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      { $set: { text } },
+      { returnDocument: "after" }
+    )
+    .then((result) => result.value)
+    .then(mapOptionalTweet);
 }
 
 export async function remove(id) {
-  return Tweet.findByPk(id).then((tweet) => {
-    tweet.destroy();
-  });
+  return getTweets().deleteOne({ _id: new ObjectId(id) });
+}
+
+function mapOptionalTweet(tweet) {
+  return tweet ? { ...tweet, id: tweet._id.toString() } : tweet;
+}
+
+function mapTweets(tweets) {
+  return tweets.map(mapOptionalTweet);
 }
